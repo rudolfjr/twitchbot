12f3a7e939dd99c7156d9100fc36dabc96b7df2b
Ricardo Godoy <rq.godoy@gmail.com>
2020-08-24 19:03:07 -0300

    Move sensible data to .env

--- bot.js
+++ bot.js
@@ -1,44 +1,42 @@
 const tmi = require('tmi.js');
-const NOME_DO_BOT = 'BAGUERA'
-const NOME_DO_CANAL_QUE_O_BOT_VAI_FICAR = 'rudolfkrjunior'
-const O_TOKEN_DO_PASSO_4 = ''
-
+require('dotenv').config();
 
 // Definir opções de configuração
 const opts = {
-    identity: {
-      username: NOME_DO_BOT,
-      password: O_TOKEN_DO_PASSO_4
-    },
-    channels: [NOME_DO_CANAL_QUE_O_BOT_VAI_FICAR]
-  };
-  // Cria um cliente tmi com  nossas opções
-  const client = new tmi.client(opts);
-  //intercepta mensagem do chat
-  function mensagemChegou(alvo, contexto, mensagem, ehBot) {
-    if (ehBot) {
-      return;
-    } //se for mensagens do nosso bot ele não faz nada
-  
-    // remove espaço em branco da mensagem para verificar o comando
-    const nomeDoComando = mensagem.trim();
-    // checando o nosso comando
-    if (nomeDoComando === '!comandoUM') {
-      client.say(alvo, `Rodando o comando ${nomeDoComando}`);
-    } else if (nomeDoComando === '!comandoDOIS') {
-      client.say(alvo, `Rodando o comando ${nomeDoComando}`);
-    } else {
-      console.log(`* Não conheço o comando ${nomeDoComando}`);
-    }
+  identity: {
+    username: process.env.NOME_DO_BOT,
+    password: process.env.O_TOKEN_DO_PASSO_4,
+  },
+  channels: [process.env.NOME_DO_CANAL_QUE_O_BOT_VAI_FICAR],
+};
+
+// Cria um cliente tmi com  nossas opções
+const client = new tmi.client(opts);
+// intercepta mensagem do chat
+function mensagemChegou(alvo, contexto, mensagem, ehBot) {
+  if (ehBot) {
+    return;
+  } // se for mensagens do nosso bot ele não faz nada
+
+  // remove espaço em branco da mensagem para verificar o comando
+  const nomeDoComando = mensagem.trim();
+  // checando o nosso comando
+  if (nomeDoComando === '!comandoUM') {
+    client.say(alvo, `Rodando o comando ${nomeDoComando}`);
+  } else if (nomeDoComando === '!comandoDOIS') {
+    client.say(alvo, `Rodando o comando ${nomeDoComando}`);
+  } else {
+    console.log(`* Não conheço o comando ${nomeDoComando}`);
   }
-  
-  function entrouNoChatDaTwitch(endereco, porta) {
-    console.log(`* Bot entrou no endereço ${endereco}:${porta}`);
-  }
-  
-  // Registra nossas funções
-  client.on('message', mensagemChegou);
-  client.on('connected', entrouNoChatDaTwitch);
-  
-  // Connecta na Twitch:
-  client.connect();
+}
+
+function entrouNoChatDaTwitch(endereco, porta) {
+  console.log(`* Bot entrou no endereço ${endereco}:${porta}`);
+}
+
+// Registra nossas funções
+client.on('message', mensagemChegou);
+client.on('connected', entrouNoChatDaTwitch);
+
+// Connecta na Twitch:
+client.connect();
